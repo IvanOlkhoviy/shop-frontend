0ad2be7847ab955c617b3d79aff47dc362a7984f
IvanOlkhoviy <ivanolkhoviy@gmail.com>
2020-09-17 22:40:31 +0300

    Created component Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+
+export const Navbar:React.FC = () =>(
+//     <nav className="z-depth-0 ">
+//     <div className="nav-wrapper white">
+//       <a href="#" className="brand-logo black-text">Logo</a>
+//       <ul  className="right hide-on-med-and-down">
+//         <li><a href="/" className="black-text">Home</a></li>
+//         <li><a href="/" className="black-text">Shop All</a></li>
+//         <li><a href="/" className="black-text">About</a></li>
+//         <li><a href="/" className="black-text">Log In</a></li>
+//         <li><a href="/" className="grey-text"><i className="large material-icons">shopping_basket</i></a></li>
+//       </ul>
+//     </div>
+//   </nav>
+<div className="container">
+    <nav className="nav justify-content-end">  
+        <a className="navbar-brand mr-auto" href="#">Logo</a>
+        <li className="nav-item nav-link "><a href="/" >Home</a></li>
+        <li className="nav-item nav-link"><a href="/" >Shop All</a></li>
+        <li className="nav-item nav-link"><a href="/" >About</a></li>
+        <li className="nav-item nav-link"><a href="/" className="">Log In</a></li>
+        <li className="nav-item nav-link">
+        <a href="/" ><svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-bag"
+            fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M8 1a2.5 2.5 0 0 0-2.5 2.5V4h5v-.5A2.5 2.5 0 0 0 8 1zm3.5 3v-.5a3.5
+             3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V5H2z"/>
+        </svg>
+        </a>
+        </li>
+    </nav>
+</div>
+)
